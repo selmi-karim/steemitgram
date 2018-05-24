9adb5ceb5c8d446448afa4c969399a9364976d8c
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-24 15:03:09 +0100

    update

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native'
 
-export default class Profile extends Component {      
+export default class Profile extends Component {   
+    
+    static navigationOptions = {
+        title: 'haaa',
+    };   
     render() {
             return( 
                 <Text> Profile Page </Text>
             )
         } 
 }
-
