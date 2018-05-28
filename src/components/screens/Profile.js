2d68acfc35154c20a0da9a16e9b84acc4814758e
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-28 12:08:59 +0100

    update

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native'
 
 export default class Profile extends Component {   
-    static navigationOptions = {
-        tabBarIcon: () => (
-          <Image
-            source={require('../../../assets/arrow.png')}
-            style={CommonStyles.tabBarIcon}
-          />
-        )
-    }
     render() {
             return( 
                 <Text> Profile Page </Text>
