3759d762f96d33d1878a6d264d222525dbc8b0cc
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-29 14:55:52 +0100

    update

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native'
 
 export default class Profile extends Component {   
+
+    constructor() {
+        super() 
+        this.state = {
+
+        }
+        }
+    }
+
     render() {
             return( 
                 <Text> Profile Page </Text>
