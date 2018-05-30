eacdcee58da18834dfabe4e0998ffc60f4fc17aa
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-30 14:14:03 +0100

    fix

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, View, Image, StyleSheet } from 'react-native'
-import SearchableFlatlist from "searchable-flatlist";
 import config from '../config/index'
-
-const data = [
-    { id: 1, name: "Francesco Raoux" },
-    { id: 2, name: "Tasha Bonanno" },
-    { id: 3, name: "Merle Braunstein" },
-    { id: 4, name: "Aleda Bouzan" },
-    { id: 5, name: "Issiah Elnaugh" }
-  ];
-  
 export default class Profile extends Component {  
     
     static navigationOptions = {
@@ -21,48 +11,16 @@ export default class Profile extends Component {
           />
         ),
     };
-    state = { searchTerm: "" };
+
     render() {
-      let { sContainer, sSearchBar, sTextItem } = styles;
-      return (
-        <View style={sContainer}>
-          <TextInput
-            placeholder={"Search"}
-            style={sSearchBar}
-            onChangeText={searchTerm => this.setState({ searchTerm })}
-          />
-          <SearchableFlatlist
-            searchProperty={"name"}
-            searchTerm={this.state.searchTerm}
-            data={data}
-            containerStyle={{ flex: 1 }}
-            renderItem={({ item }) => <Text style={sTextItem}>{item.name}</Text>}
-            keyExtractor={item => item.id}
-          />
-        </View>
-      );
-    }
+            return( 
+                <Text> Profile Page </Text>
+            )
+        } 
 }
+
+
 const styles = StyleSheet.create({
-    sContainer: {
-        flex: 1,
-        backgroundColor: "#F5FCFF"
-    },
-    sTextItem: {
-        height: 50,
-        width: "100%",
-        textAlign: "center",
-        textAlignVertical: "center",
-        fontSize: 18
-    },
-    sSearchBar: {
-        paddingHorizontal: 10,
-        margin: 10,
-        height: 50,
-        borderColor: "gray",
-        borderWidth: 1,
-        fontSize: 18
-    },
     barIcon: {
       width: 26,
       height: 26,
