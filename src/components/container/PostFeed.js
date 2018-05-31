916df6df6a9e85f08e1a207bd9ed36555beb42f5
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-31 15:01:56 +0100

    accueil

--- src/components/container/PostFeed.js
+++ src/components/container/PostFeed.js
@@ -7,9 +7,36 @@ export default class PostFeed extends Component {
     constructor(){
         super();
         this.state = {
-          data: [1,2,3,4,5,6,7],
+          page: 1,
+          data: [],
         };
     }
+
+    async fetchData(page) {
+    const uri = "https://randomuser.me/api/";
+    const response = await fetch(
+        `${uri}?page=${page}&results=${this.state.results}&seeds=${this.state
+        .seed}`
+    );
+    const jsondata = await response.json();
+    return jsondata.results;
+    }
+
+    async loadData(page) {
+    this.setState({ isFetching: true });
+    const data = await this.fetchData(page);
+    const nextPage = page + 1;
+    const formatedData = this.fromArrayToSectionData(data);
+    this.setState({
+        page: nextPage,
+        data: [...this.state.data, ...data],
+    });
+    }
+    
+    async componentDidMount() {
+        await this.loadData(this.state.page);
+    }
+
     
     _renderPost({item}) {
         return <Post item={item} />
