712545f1006ae855532ec08911988aca93c875c5
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-05 14:24:06 +0100

    add options for tab navigation

--- src/Insta.js
+++ src/Insta.js
@@ -2,30 +2,30 @@
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-05-07 10:31:50 
  * @Last Modified by: kerim-selmi, karimation
- * @Last Modified time: 2018-06-01 10:34:43
+ * @Last Modified time: 2018-06-05 14:23:05
  */
 
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { createSwitchNavigator, createBottomTabNavigator } from 'react-navigation'
-import { MainFeed, Login, Profile, CameraEx, Search, Favorite  } from './components/screens';
+import { MainFeed, Login, Profile, CameraEx, Search, Favorite } from './components/screens';
 
 const Tabs = createBottomTabNavigator({
-        feed: MainFeed,
-        search: Search,
-        camera: CameraEx,
-        favorite: Favorite,
-        profile: Profile
-    }
-    ,{
+    feed: MainFeed,
+    search: Search,
+    camera: CameraEx,
+    favorite: Favorite,
+    profile: Profile
+}
+    , {
         tabBarOptions: {
             showLabel: false,
             showIcon: true
         }
-})
+    })
 
 const MainStack = createSwitchNavigator({
-    main: Tabs, 
+    main: Tabs,
     login: Login
 })
 
