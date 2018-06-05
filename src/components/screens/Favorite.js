85f5ea4a5043cb049edeaefd265015a89e84b818
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-05 12:32:47 +0100

    user fav image

--- src/components/screens/Favorite.js
+++ src/components/screens/Favorite.js
@@ -1,13 +1,15 @@
 /*
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-06-01 10:33:17 
- * @Last Modified by:   kerim-selmi, karimation 
- * @Last Modified time: 2018-06-01 10:33:17 
+ * @Last Modified by: kerim-selmi, karimation
+ * @Last Modified time: 2018-06-05 12:28:46
  */
 
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, View, Image, StyleSheet } from 'react-native'
 import config from '../config/index'
+import {FavFeed} from './../container'
+
 export default class Favorite extends Component {  
     
     static navigationOptions = {
@@ -20,8 +22,10 @@ export default class Favorite extends Component {
     };
 
     render() {
-            return( 
-                <Text> fav Page </Text>
+            return(
+                <View style={{paddingTop:40}}> 
+                    <FavFeed />
+                </View>
             )
         } 
 }
