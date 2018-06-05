241dfd798047f736bdd9af1cd1845ee25d3e5d9c
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-05 12:32:17 +0100

    add Fav file

--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -1,14 +1,15 @@
 /*
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-06-01 10:33:35 
- * @Last Modified by:   kerim-selmi, karimation 
- * @Last Modified time: 2018-06-01 10:33:35 
+ * @Last Modified by: kerim-selmi, karimation
+ * @Last Modified time: 2018-06-05 12:24:06
  */
 
- import Post from './Post.js'
+import Post from './Post.js'
 import DisplayUser from './DisplayUser'
-
+import Fav from './Fav'
 export  {
     Post,
-    DisplayUser
+    DisplayUser,
+    Fav
 }
