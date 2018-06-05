b1e0b190f4b04e34c79344dac1f19bd85c2acbab
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-05 14:26:53 +0100

    add header file

--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -2,14 +2,16 @@
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-06-01 10:33:35 
  * @Last Modified by: kerim-selmi, karimation
- * @Last Modified time: 2018-06-05 12:24:06
+ * @Last Modified time: 2018-06-05 13:19:51
  */
 
 import Post from './Post.js'
 import DisplayUser from './DisplayUser'
 import Fav from './Fav'
+import Header from './Header'
 export  {
     Post,
     DisplayUser,
-    Fav
+    Fav,
+    Header
 }
