73a090d2dda17a23c849d8efc0e7b970cf278c6b
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-05 14:27:28 +0100

    update

--- src/components/screens/MainFeed.js
+++ src/components/screens/MainFeed.js
@@ -2,53 +2,40 @@
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-05-22 10:32:36 
  * @Last Modified by: kerim-selmi, karimation
- * @Last Modified time: 2018-06-01 10:40:05
+ * @Last Modified time: 2018-06-05 13:20:22
  */
 
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
 import config from './../config/index'
-import {PostFeed} from './../container'
+import { PostFeed } from './../container'
+import { Header } from './../presentation'
 
-export default class MainFeed extends Component {      
+export default class MainFeed extends Component {
 
 
     static navigationOptions = {
         tabBarIcon: ({ tintColor }) => (
-          <Image
-            source={config.images.accueil}
-            style={[styles.barIcon, {tintColor: tintColor}]}
-          />
+            <Image
+                source={config.images.accueil}
+                style={[styles.barIcon, { tintColor: tintColor }]}
+            />
         ),
     };
 
     render() {
-            return( 
-                <View style={{ flex:1,width:100+'%',height:100+'%' }}>
-                    {/* header: app name,dimension */}
-                    <View 
-                        style={styles.tempNav} >
-                        <Text styltouche={{ fontSize:20 }}>SteemitGramm </Text>
-
-                    </View>
-                {/* home page with some posts (randomly posts)*/}    
-                <PostFeed />    
-                </View>
-            )
-        }
+        return (
+            <View style={{ flex: 1, width: 100 + '%', height: 100 + '%' }}>
+                {/* header: app name,dimension */}
+                <Header />
+                {/* home page with some posts (randomly posts)*/}
+                <PostFeed />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-    tempNav: {
-        width:100+'%',
-        height:45  , 
-        backgroundColor:'#e3e4e5',
-        borderBottomColor: '#fff',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        justifyContent: 'center',
-        alignItems:'center',
-        marginTop: 20
-    },
     barIcon: {
         width: 26,
         height: 26,
