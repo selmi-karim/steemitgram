25717a756a210a7279828fa0a4466c925eabd6e6
Selmi11895480 <selmi.kerim@gmail.com>
2018-06-06 15:23:04 +0100

    bug in rn-material-menu

--- src/components/presentation/Header.js
+++ src/components/presentation/Header.js
@@ -2,27 +2,55 @@
  * @Author: kerim-selmi, karimation 
  * @Date: 2018-06-05 13:17:33 
  * @Last Modified by: kerim-selmi, karimation
- * @Last Modified time: 2018-06-06 11:47:05
+ * @Last Modified time: 2018-06-06 15:22:04
  */
 
 
-import React from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import React from "react"
+import { View, Text, Image, StyleSheet, TouchableOpacity, Alert, TouchableHighlight } from "react-native"
+import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu'
 import config from '../config/index'
 /*
  * users details 
  * feature: add clicked button -> user profile
  */
+_menu = null;
+
+setMenuRef = ref => {
+    this._menu = ref;
+};
+
+hideMenu = () => {
+    this._menu.hide();
+};
+
+showMenu = () => {
+    this._menu.show();
+};
 export default (DisplayUser = props => (
     <View
         style={styles.tempNav} >
-        <Text styltouche={{ fontSize: 23 }} style={{position: 'absolute'}}> SteemitGramm </Text>
-        <View style={{flexDirection: 'row',
-            marginLeft: 'auto'}} >
-            <TouchableOpacity onPress={() => { Alert.alert('soon: profile config') }} >
-                <Image style={styles.configIcon} source={config.images.param} />
-            </TouchableOpacity>    
+        <Text styltouche={{ fontSize: 23 }} style={{ position: 'absolute' }}> SteemitGramm </Text>
+        <View style={{
+            flexDirection: 'row',
+            marginLeft: 'auto'
+        }} >
+           
         </View>
+        <Menu
+          ref={this.setMenuRef}
+          button={<TouchableHighlight onPress={this.showMenu}>
+          <Image
+            style={styles.configIcon}
+            source={config.images.param}
+          />
+        </TouchableHighlight>}
+        >
+          <MenuItem onPress={this.hideMenu}>Menu item 1</MenuItem>
+          <MenuItem onPress={this.hideMenu}>Menu item 2</MenuItem>
+          <MenuDivider />
+          <MenuItem onPress={this.hideMenu}>Menu item 4</MenuItem>
+        </Menu>
     </View>
 ));
 
@@ -43,5 +71,5 @@ const styles = StyleSheet.create({
         width: 25,
         borderRadius: 20,
         marginRight: 8
-        },
+    },
 });
