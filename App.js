df760efff86a56a86e5b44ce280dc453658f7eb5
karimation <selmi.kerim@gmail.com>
2018-08-07 17:14:12 +0200

    steem signin added :bulb:

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Insta from './src/Insta.js'
+import React, { Component } from 'react'
+import { Text, View, Alert } from 'react-native'
+import SteemConnect from 'react-native-steem-signin'
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  _signIn(auth) {
+    Alert.alert('Auth data: ' + auth);
+  }
+
   render() {
     return (
-        <Insta />
+      <SteemConnect
+        onLoggedIn={(auth) => this._signIn(auth)}
+        btnWidth={180}
+        btnHeight={80}
+        fontFamily={'System'}
+      />
     );
   }
 }
