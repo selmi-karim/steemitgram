c5153b7ab9942458f69d7ad79451b328efed0622
karimation <selmi.kerim@gmail.com>
2018-08-17 16:36:59 +0200

    latech description

--- src/components/config/index.js
+++ src/components/config/index.js
@@ -5,42 +5,37 @@
  * @Last Modified time: 2018-06-06 10:26:40
  */
 import { Dimensions } from 'react-native'
-
- // static resources (img,styles,color ...)
-
-export default{
-    // static images
+export default {
     text: {
         latech: "LaTech is a French company with extensive experience in local project management and nearshore mode. Beyond the customer / supplier relationship, LaTech develops partnerships and commits to the success of projects by adopting an approach based on cooperation and transparency.",
-        privacyAndSecurity: 'sdqsddq'
-    },
+    }, 
     images: {
         heartIcon: require('../../../assets/heart.png'),
         bubbleIcon: require('../../../assets/bubble.png'),
-        arrowIcon: require('../../../assets/arrow.png'), 
-        blackHeartIcon: require('../../../assets/blackHeart.png'), 
-        profile: require('../../../assets/profile.png'), 
-        search: require('../../../assets/search.png'), 
-        camera: require('../../../assets/camera.png'), 
-        accueil: require('../../../assets/accueil.png'), 
-        fav: require('../../../assets/fav.png'), 
-        param: require('../../../assets/config.png'), 
-        upArrow: require('../../../assets/uparrow1.png'), 
-        plus: require('../../../assets/plus.png'), 
-        plusB: require('../../../assets/plus-b.png'), 
-        notif: require('../../../assets/notif.png'), 
-        location: require('../../../assets/location.png'), 
-        website: require('../../../assets/website.png'), 
-        user: require('../../../assets/user.png'), 
-        power: require('../../../assets/power.png'), 
-        latech: require('../../../assets/latech.png'), 
-        
+        arrowIcon: require('../../../assets/arrow.png'),
+        blackHeartIcon: require('../../../assets/blackHeart.png'),
+        profile: require('../../../assets/profile.png'),
+        search: require('../../../assets/search.png'),
+        camera: require('../../../assets/camera.png'),
+        accueil: require('../../../assets/accueil.png'),
+        fav: require('../../../assets/fav.png'),
+        param: require('../../../assets/config.png'),
+        upArrow: require('../../../assets/uparrow1.png'),
+        plus: require('../../../assets/plus.png'),
+        plusB: require('../../../assets/plus-b.png'),
+        notif: require('../../../assets/notif.png'),
+        location: require('../../../assets/location.png'),
+        website: require('../../../assets/website.png'),
+        user: require('../../../assets/user.png'),
+        power: require('../../../assets/power.png'),
+        latech: require('../../../assets/latech.png'),
+
     },
     // static styles
     styleConstants: {
         rowHeight: 50,
         screenWidth: Dimensions.get('window').width,
         screenHeight: Dimensions.get('window').height,
-        halfWidth: Dimensions.get('window').width/2
-    }  
+        halfWidth: Dimensions.get('window').width / 2
+    }
 }
