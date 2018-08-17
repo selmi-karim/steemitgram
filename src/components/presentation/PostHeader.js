6267ebc124025ab317766582152996abc197e497
karimation <selmi.kerim@gmail.com>
2018-08-17 11:31:19 +0200

    about as dialog

--- src/components/presentation/PostHeader.js
+++ src/components/presentation/PostHeader.js
@@ -6,63 +6,112 @@
  */
 
 
-import React from "react"
-import { View, Text, Image, StyleSheet, TouchableOpacity, AsyncStorage, BackAndroid } from "react-native"
+import React, { PureComponent } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, AsyncStorage, BackAndroid, Button, TextInput } from "react-native"
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu'
 import config from '../config/index'
-import { Header } from "react-native-elements";
+import { Dialog } from 'react-native-simple-dialogs';
+
 /*
  * users details 
  * feature: add clicked button -> user profile
  */
-_menu = null;
 
-setMenuRef = ref => {
-    this._menu = ref;
-};
-
-
-hideMenu = () => {
-    this._menu.hide();
-};
-
-exit = async () => {
-    try {
-        await AsyncStorage.clear();
-    } catch (error) {
+export default class PostHeader extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+        }
     }
-    BackAndroid.exitApp()
+
+    _menu = null;
+
+    setMenuRef = ref => {
+        this._menu = ref;
+    }
+
+
+    hideMenu = () => {
+        this._menu.hide()
+    }
+
+    privacyAndSecurity = () => {
+        this._menu.hide()
+    }
+
+    AboutAs = () => {
+        this._menu.hide()
+        this.aboutAs(true)
+    }
+
+    reportBug = () => {
+        this._menu.hide()
+    }
+
+    exit = async () => {
+        this._menu.hide()
+        try {
+            await AsyncStorage.clear();
+        } catch (error) {
+        }
+        BackAndroid.exitApp()
+    }
+
+    aboutAs = (show) => {
+        this.setState({ aboutAs: show })
+    }
+
+    showMenu = () => {
+        this._menu.show();
+    };
+    render() {
+        return (
+            <View
+                style={styles.tempNav} >
+                <Text styltouche={{ fontSize: 23 }} style={{ position: 'absolute' }}> SteemitGramm </Text>
+                <View style={{
+                    flexDirection: 'row',
+                    marginLeft: 'auto'
+                }} >
+
+                </View>
+                <Menu
+                    ref={this.setMenuRef}
+                    button={<TouchableOpacity onPress={this.showMenu}>
+                        <Image
+                            style={styles.configIcon}
+                            source={config.images.param}
+                        />
+                    </TouchableOpacity>}
+                >
+                    <MenuItem onPress={this.privacyAndSecurity}>Privacy & Security</MenuItem>
+                    <MenuItem onPress={this.reportBug}>Report Bug</MenuItem>
+                    <MenuItem onPress={this.AboutAs}>About as</MenuItem>
+                    <MenuDivider />
+                    <MenuItem onPress={this.exit}>Exit</MenuItem>
+                </Menu>
+                <Dialog
+                    visible={this.state.aboutAs}
+                    title="Latech"
+                    onTouchOutside={() => this.aboutAs(false)}
+                    contentStyle={{ justifyContent: 'center', alignItems: 'center', }}
+                    animationType="fade">
+                    <Image
+                        source={config.images.latech}
+                        resizeMode="contain"
+                        resizeMethod="scale"
+                        style={{ marginBottom: 10, height: 50, width: 100 }} />
+                    <Text style={{ marginBottom: 10 }}>LaTech est une société française avec une vaste expérience dans
+                     la gestion de projets locaux et le mode nearshore. Au-delà de la relation client / fournisseur,
+                     LaTech développe des partenariats et s'engage pour la réussite des projets en adoptant une approche
+                     basée sur la coopération et la transparence.</Text>
+                    <Button onPress={() => this.aboutAs(false)} style={{ marginTop: 10 }} title="CLOSE" />
+                </Dialog>
+
+            </View>
+        )
+    }
 }
-
-showMenu = () => {
-    this._menu.show();
-};
-export default (PostHeader = props => (
-    <View
-        style={styles.tempNav} >
-        <Text styltouche={{ fontSize: 23 }} style={{ position: 'absolute' }}> SteemitGramm </Text>
-        <View style={{
-            flexDirection: 'row',
-            marginLeft: 'auto'
-        }} >
-
-        </View>
-        <Menu
-            ref={this.setMenuRef}
-            button={<TouchableOpacity onPress={this.showMenu}>
-                <Image
-                    style={styles.configIcon}
-                    source={config.images.param}
-                />
-            </TouchableOpacity>}
-        >
-            <MenuItem onPress={this.hideMenu}>Menu item 1</MenuItem>
-            <MenuItem onPress={this.hideMenu}>Menu item 2</MenuItem>
-            <MenuDivider />
-            <MenuItem onPress={this.exit}>Exit</MenuItem>
-        </Menu>
-    </View>
-));
 
 const styles = StyleSheet.create({
     tempNav: {
